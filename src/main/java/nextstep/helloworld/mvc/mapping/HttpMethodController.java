c4ab45dae36f574c39362bfa90b625ade841a32e
dwl5 <ssop6403@gmail.com>
2021-04-13 17:13:23 +0900

    feat: HttpMethod 구현

--- src/main/java/nextstep/helloworld/mvc/mapping/HttpMethodController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/HttpMethodController.java
@@ -1,6 +1,7 @@
 package nextstep.helloworld.mvc.mapping;
 
 import nextstep.helloworld.mvc.domain.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,11 +12,14 @@ import java.util.List;
 @RestController
 public class HttpMethodController {
 
+    @PostMapping(value = "/http-method/users")
     public ResponseEntity createUser(@RequestBody User user) {
+        System.out.println("user : " + user);
         Long id = 1L;
         return ResponseEntity.created(URI.create("/users/" + id)).build();
     }
 
+    @GetMapping(value = "/http-method/users")
     public ResponseEntity<List<User>> showUser() {
         List<User> users = Arrays.asList(
                 new User("이름", "email"),
