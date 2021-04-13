0b96862a38282b5a60fa5ca53960d3784240dd91
dwl5 <ssop6403@gmail.com>
2021-04-14 00:13:20 +0900

    feat: MediaTypeTest 구현

--- src/main/java/nextstep/helloworld/mvc/mapping/MediaTypeController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/MediaTypeController.java
@@ -13,20 +13,23 @@ import java.util.List;
 @RequestMapping("/media-type")
 public class MediaTypeController {
 
+    @PostMapping(path = "/users", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity createUser(@RequestBody User user) {
         Long id = 1L;
         return ResponseEntity.created(URI.create("/users/" + id)).build();
     }
 
+    @GetMapping(path = "/users", produces = MediaType.TEXT_HTML_VALUE)
+    public String userPage() {
+        return "user page";
+    }
+
+    @GetMapping(path = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<User>> showUser() {
         List<User> users = Arrays.asList(
                 new User("이름", "email"),
                 new User("이름", "email")
         );
         return ResponseEntity.ok().body(users);
-    }
-
-    public String userPage() {
-        return "user page";
     }
 }
